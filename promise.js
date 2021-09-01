644553aff7e12ea8484c86f99935ce01cb1c6693
Pulpo-julian <julian.schrodinger@gmail.com>
2021-09-01 17:35:34 -0500

    añado promise.js

--- promise.js
+++ promise.js
@@ -1,11 +1,31 @@
-const f = () => new Promise((resolve, reject) => {
-    const err = false;
-    if (err) {
-        reject('Error');
-    } else {
-        resolve('Value');
-    }
+const url = "https://jsonplaceholder.typicode.com";
+
+const getUser = (id) => new Promise((resolve, reject) => {
+    request( {"uri": `${url}/users/${id}`}, (err, res, body) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve( JSON.parse(body) );
+        }
+    } )
 });
 
-const p = f();
-console.log(p);
+const getPosts = (id) => new Promise((resolve, reject) => {
+    request( {"uri": `${url}/posts?userId=${id}&_limit=3`}, (err, res, body) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve( JSON.parse(body) );
+        }
+    } )
+});
+
+getUser(1)
+    .then(users => {
+        const user = users[0];
+        console.log(user);
+        return user;
+    })
+    .then(user => getPosts(user.id))
+    .then(posts => console.log(posts))
+    .catch(e => console.error(e));
